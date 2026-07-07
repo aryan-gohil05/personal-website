f3d1628d0e1dbe45b10b492d9c477f31584af1da
Aryan Gohil <117673260+aryan-gohil05@users.noreply.github.com>
2026-07-07 15:31:07 +0100

    Put book review content into seperate file
    Fixed URL slugs for book reviews

---
+++ worth-your-time/books.js
@@ -0,0 +1,57 @@
+const books = [
+    {
+        id: "The-Millionaire-Fastlane",
+        title: "The Millionaire Fastlane",
+        author: "M.J. DeMarco",
+        cover: "/images/books/fastlane_cover.jpg",
+        link: "https://www.waterstones.com/book/the-millionaire-fastlane/mj-demarco/9780984358106",
+        review: [
+            "## Impressions",
+            "This book completely changed how I think about money and time; it’s the reason I became interested in entrepreneurship.",
+            "## The Slowlane vs The Fastlane",
+            "At the start, MJ DeMarco uses logic and maths to describe the <em>‘Slowlane’</em>, a financially mediocre plan where you live poor young and then die rich old. Trading time for money typically sounds like this:",
+            "<q>Go to university -> get good grades -> graduate -> get a good job -> save 20% of your pay check -> invest in the stock market, preferably in a low-cost index fund -> max your pension contributions -> keep a frugal budget… then someday, when you are 65 years old or better, 55 years old if you are super miserly, you will be rich.</q>",
+            "Later in the book, he uses business as a <em>‘Fastlane’</em> vehicle to arrive at two destinations:",
+            "<ol><li>Complete passivity with a money system</li><li>An exit strategy where you liquidate huge high-value assets for millions of pounds</li></ol>",
+            "Thus, achieving financial freedom within <strong>5-10 years</strong>.",
+            "## The 'CENTS' framework to picking the right business",
+            "To judge whether a business idea actually qualifies as a Fastlane vehicle, he introduces a framework called CENTS (Control, Entry, Need, Time, Scale):",
+            "<ul><li><strong>Control</strong> - You run the system, not an employer or platform that can pull the rug out from under you.</li><li><strong>Entry</strong> - Businesses that anyone can easily start in minutes, like drop shipping or affiliate marketing, are not worth it. The higher the barrier to entry, the more likely you are to make money, as there’s less competition. Example: There are only a few chip foundries, like TSMC, in the world, as it’s a tough business. Therefore, these companies make hundreds of billions of pounds.</li><li><strong>Need</strong> - Stop chasing money and start chasing market needs. Solve a real problem people will pay for, not something that you selfishly want.</li><li><strong>Time</strong> - Build a company that can eventually run without you directly working on it. You build the product or do the work once so it can be constantly sold over and over to generate income while you sleep. Examples: <ul><li>Digital products: Books, YT vids, website / coding templates</li><li>Software / internet systems (what I’m trying to do): SaaS, video games, apps</li><li>Renting out stuff: Real estate, cars, tools, etc.</li><li>Physical products: Skincare brand, clothing line</li></ul></li><li><strong>Scale</strong> - If you can sell to millions, you can make millions. Does the business affect only your local community or the world?</li></ul>",
+            "## How the book helped me",
+            "I use CENTS to quickly validate business ideas I have. Seeing my own ideas fail this test has saved me a lot of time on what I should focus on.",
+            "A local service business might satisfy <strong>Need</strong> but fail to <strong>Scale</strong>. A software idea might satisfy <strong>Entry</strong> but fail <strong>Control</strong> if it’s fully dependent on someone else’s platform. Like an online store that ONLY exists on Shopify or Amazon. Since Amazon could suspend the account or launch a competing product at any time and I’d be out of business.",
+            "I will admit his language is abrasive, especially at the start of the book, but if you can get past that, it’s genuinely worth reading.",
+            "I always recommend this book to anyone who is thinking of <strong>launching a business</strong> or taking there financial future into their own hands."
+        ]
+    },
+    {
+        id: "The-Black-Hole-War",
+        title: "The Black Hole War",
+        author: "Leonard Susskind",
+        cover: "/images/books/black-hole-war.jpg",
+        link: "https://www.waterstones.com/book/the-black-hole-war/leonard-susskind/9780316016414",
+        review: [
+            "Writing this up. Just give me a sec"
+        ]
+    },
+    {
+        id: "book-3",
+        title: "Hitchhiker's Guide to the Galaxy - Volume 1",
+        author: "Douglas Adams",
+        cover: "/images/books/hitchhikers-1.jpg",
+        link: "https://www.waterstones.com/book/the-hitchhikers-guide-to-the-galaxy/douglas-adams/9781529034523",
+        review: [
+            "Writing this up. Just give me a sec"
+        ]
+    },
+    {
+        id: "book-4",
+        title: "Atomic Habits",
+        author: "James Clear",
+        cover: "/images/books/atomic-habits.jpg",
+        link: "https://www.waterstones.com/book/atomic-habits/james-clear/9781847941831",
+        review: [
+            "I want to read this. I've heard good things"
+        ]
+    }
+];
